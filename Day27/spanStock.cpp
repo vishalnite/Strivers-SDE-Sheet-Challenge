8eedf5002cb039286a36c82c6ae93138afa5bcd6
Vishal Kumar <klv020439@gmail.com>
2023-07-05 19:56:10 +0530

    solved span stock problem

---
+++ Day27/spanStock.cpp
@@ -0,0 +1,34 @@
+/*
+Find the number of consecutive previous days where the current stock price is greater than previous days.
+Calculate span for each day.
+
+T.C - O(N)
+S.C - O(N)
+*/
+
+#include <bits/stdc++.h> 
+vector<int> findSpans(vector<int> &price) {
+    int n = price.size();
+    vector<int> ans;
+    stack<int> s;
+
+    s.push(0);
+    ans.push_back(1);
+
+    //For each index keep a track of element greater the the current element
+    for(int i = 1; i < n; i++) {
+        while(!s.empty() && price[s.top()] <= price[i]) {
+            s.pop();
+        }
+
+        if(s.empty()) {
+            ans.push_back(i + 1);
+        }
+        else {
+            ans.push_back(i - s.top());
+        }
+        s.push(i);
+    }
+
+    return ans;
+}
