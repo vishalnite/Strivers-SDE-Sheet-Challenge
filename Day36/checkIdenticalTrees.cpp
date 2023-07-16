75adbec38e8272699ee07d815f17ba2a3b72f649
Vishal Kumar <klv020439@gmail.com>
2023-07-16 19:07:55 +0530

    solved check identical trees problem

---
+++ Day36/checkIdenticalTrees.cpp
@@ -0,0 +1,38 @@
+/*
+Find whether the two trees are identical or not.
+[Do any tree traversal simultaneously on both the trees]
+
+T.C - O(N)
+S.C - O(N)
+*/
+
+#include <bits/stdc++.h> 
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    if(root1 == NULL || root2 == NULL)
+        return (root1 == root2);
+
+    return ((root1->data == root2->data)
+        && identicalTrees(root1->left, root2->left)
+        && identicalTrees(root1->right, root2->right)); 	 
+}
