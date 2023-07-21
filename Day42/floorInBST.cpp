54eb8de907668446896c3420b762ba8b37fd0720
Vishal Kumar <klv020439@gmail.com>
2023-07-21 13:06:17 +0530

    solved floor in a BST problem

---
+++ Day42/floorInBST.cpp
@@ -0,0 +1,48 @@
+/*
+Find the element in BST which is <= the given key. (floor)
+
+T.C - O(logN)
+S.C - O(1)
+*/
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> * root, int X)
+{
+    int floor = -1;
+
+    while(root) {
+        if(root->val == X) {
+            return root->val;
+        }
+
+        if(X > root->val) {
+            floor = root->val;
+            root = root->right;
+        }
+        else {
+            root = root->left;
+        }
+    }
+
+    return floor;
+}
