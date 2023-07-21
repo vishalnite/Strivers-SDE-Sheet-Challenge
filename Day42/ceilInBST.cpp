54f81051f37246c13deda56289f17bbf6a4b8a98
Vishal Kumar <klv020439@gmail.com>
2023-07-21 12:57:25 +0530

    solved ceil in a BST problem

---
+++ Day42/ceilInBST.cpp
@@ -0,0 +1,55 @@
+/*
+Find a value in BST which is >= the given key. (ceil)
+
+T.C - O(logN)
+S.C - O(1)
+*/
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if (left) {
+              delete left;
+            }
+            if (right) {
+              delete right;
+            }
+        }
+    };
+
+************************************************************/
+
+int findCeil(BinaryTreeNode<int> *node, int x){
+    int ceil = -1;
+
+    while(node) {
+        if(node->data == x) {
+            return node->data;
+        }
+
+        if(x > node->data) {
+            node = node->right;
+        }
+        else {
+            ceil = node->data;
+            node = node->left;
+        }
+    }
+
+    return ceil;
+}
