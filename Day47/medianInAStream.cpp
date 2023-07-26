b566ec34509c88db0f134a4e97d3cb4ab1a20353
Vishal Kumar <klv020439@gmail.com>
2023-07-27 01:10:46 +0530

    solved median in a stream problem

---
+++ Day47/medianInAStream.cpp
@@ -0,0 +1,46 @@
+/*
+Find median in a running stream.
+[Use maxHeap and minHeap -- maxHeap will be greater in size then minHeap -- change accordingly]
+
+T.C - O(N)
+S.C - O(N)
+*/
+
+#include<bits/stdc++.h>
+
+vector<int> findMedian(vector<int> &arr, int n){
+	
+	vector<int> ans;
+	priority_queue<int> maxHeap;
+	priority_queue<int, vector<int>, greater<int>> minHeap;
+
+	for(int x : arr) {
+		if(maxHeap.size() == minHeap.size()) {
+			if(maxHeap.size() == 0 || x <= minHeap.top()) {
+				maxHeap.push(x);
+			}
+			else {
+				maxHeap.push(minHeap.top());
+				minHeap.pop();
+				minHeap.push(x);
+			}
+
+			ans.push_back(maxHeap.top());
+		}
+		else {
+			if(x < maxHeap.top()) {
+				minHeap.push(maxHeap.top());
+				maxHeap.pop();
+				maxHeap.push(x);
+			}
+			else {
+				minHeap.push(x);
+			}
+
+			ans.push_back((maxHeap.top() + minHeap.top()) / 2);
+		}
+	}
+
+	return ans;
+
+}
