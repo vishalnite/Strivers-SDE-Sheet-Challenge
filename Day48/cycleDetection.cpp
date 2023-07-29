c5dfec184d292e1461a5bf19c891a3d5fc7cd769
Vishal Kumar <klv020439@gmail.com>
2023-07-29 13:11:52 +0530

    solved cycle detection problem

---
+++ Day48/cycleDetection.cpp
@@ -0,0 +1,58 @@
+/*
+Detect whether the given undirected graph contains a cycle. The graph can be disconnected.
+[BFS or DFS]
+[BFS -- If two nodes collide at the same node then it contains cycle]
+
+T.C - O(N + 2E)
+S.C - O(3N)
+*/
+
+#include<queue>
+
+bool detectCycle(int src, vector<vector<int>> &adj, int vis[]) {
+    vis[src] = 1;
+    queue<pair<int, int>> q;
+    q.push({src, -1});
+
+    while(!q.empty()) {
+        int node = q.front().first;
+        int parent = q.front().second;
+        q.pop();
+
+        for(auto adjacentNode : adj[node]) {
+            if(!vis[adjacentNode]) {
+                vis[adjacentNode] = 1;
+                q.push({adjacentNode, node});
+            }
+            else if(parent != adjacentNode) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    vector<vector<int>> adj(n + 1);
+    
+    for(int i = 0; i < edges.size(); i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int vis[n + 1] = {0};
+    for(int i = 1; i <= n; i++) {
+        if(!vis[i]) {
+            if(detectCycle(i, adj, vis))
+                return "Yes";
+        }
+    }
+
+    return "No";
+
+}
